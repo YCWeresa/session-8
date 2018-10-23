7f210641bedc0ec5cd00475c9d6aed520150d380
Wang Ya Chun <xiongjack@Wang-Ya-Chun.sciences-po.fr>
2018-10-23 12:12:52 +0200

    CV

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Ya Chun",
+    lastName: "Wang",
+    description: "(I wish) Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
+    photo: "DSC_0001.jpg",
+    twitter: "YC_WANGstrollin",
+    github: "YCWeresa",
+    mail: "yachun.wang@sciencespo.fr",
     experiences: [{
-      dateBegin: "2014",
+      dateBegin: "2018",
       dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
+      name: "Tencent.Co",
+      title: "Reporter",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }, {
-      dateBegin: "Été 2013",
+      dateBegin: "Été 2017",
       dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
+      name: "RSF Taipei",
+      title: "Intern journaliste",
       description: false
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
+      dateBegin: "2015",
+      dateEnd: "2016",
+      name: "Oglivy One Taipei",
       title: "Stagiaire et correspondant de presse",
       description: false
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
-      name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2019",
+      dateEnd: "2019",
+      name: "Master Journalisme et des Affaires Internationales",
+      university: "Sciences Po Paris"
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
+      dateBegin: "2013",
+      dateEnd: "2017",
       name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      university: "Sciences Po Paris"
     }],
-    languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    languages: ["Anglais, Mandarin, Français"],
   },
   computed: {
     fullName() {
